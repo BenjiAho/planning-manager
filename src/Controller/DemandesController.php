2322c15a78ccc48edb2be046b4e25d826180e37a
BenjiAho <benjamin.aho-nienne@le-campus-numerique.fr>
2021-11-28 19:03:57 +0100

    DemandesController.php test fetch some data

--- src/Controller/DemandesController.php
+++ src/Controller/DemandesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Demandes;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,24 @@ use Symfony\Component\Routing\Annotation\Route;
 class DemandesController extends AbstractController
 {
     /**
-     * @Route("/demandes", name="demandes")
+     * @Route("/demandes/{id}", name="demandes")
      */
-    public function index(): Response
+    public function createProduct(int $ID): Response
     {
-        return $this->render('demandes/index.html.twig', [
-            'controller_name' => 'DemandesController',
-        ]);
+        $demandes = $this->getDoctrine()
+            ->getRepository(Demandes::class)
+            ->find($ID);
+
+        if (!$demandes) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$ID
+            );
+        }
+
+//        return new Response('Check out this great product: '.$demandes->getName());
+
+        // or render a template
+        // in the template, print things with {{ product.name }}
+         return $this->render('demandes/index.html.twig', ['demandes' => $demandes]);
     }
 }

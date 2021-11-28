2cc8d9b63efb6fb9abc186bd35b3a49035232408
BenjiAho <benjamin.aho-nienne@le-campus-numerique.fr>
2021-11-28 15:59:19 +0100

    abstract controller

--- src/Controller/DemandesController.php
+++ src/Controller/DemandesController.php
@@ -4,10 +4,28 @@ namespace App\Controller;
 
 use App\Entity\Demandes;
 use App\Form\Type\DemandesType;
-use http\Env\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 
-class DemandesController extends AbstractApiController
+class DemandesController extends AbstractController
 {
+    public function showDemandes(int $id): Response
+    {
+        $product = $this->getDoctrine()
+            ->getRepository(Demandes::class)
+            ->find($id);
 
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        return new Response('Check out this great product: '.$product->getName());
+
+        // or render a template
+        // in the template, print things with {{ product.name }}
+        // return $this->render('product/show.html.twig', ['product' => $product]);
+    }
 }
